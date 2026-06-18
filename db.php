3afaddf8f9261a8e39ce660ef0188f4e8d78beb0
SUNANDA <282883131+Sunanda-x@users.noreply.github.com>
2026-06-18 17:08:26 +0530

    Refactor database connection error handling
    Improve error message for database connection failure.

--- db.php
+++ db.php
@@ -1,15 +1,9 @@
 <?php
 
-$conn = new mysqli(
-    "localhost",
-    "root",
-    "",
-    "feedback_system"
-);
+$conn = new mysqli("localhost", "root", "", "feedback_system");
 
-if($conn->connect_error)
-{
-    die("Connection Failed");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
 ?>
